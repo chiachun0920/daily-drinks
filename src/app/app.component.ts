b76452e6f9b35064ee0976859b8ddef2febf7331
Chris Chang <chris@chris.local>
2019-04-16 00:46:39 +0800

    implement modal to add additional notes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,13 @@ export class AppComponent implements OnInit {
   menu: Menu;
   orders: any[] = [];
 
+  isShowModal: boolean = false;
+
+  ice: string = "正常冰";
+  sugar: string = "正常糖";
+
+  modalResult;
+
   constructor(private app: AppService) {}
 
   ngOnInit() {
@@ -25,11 +32,31 @@ export class AppComponent implements OnInit {
     })
   }
 
-  onAdd(event) {
-    this.orders.push({ ...event });
+  async onAdd(event) {
+    try {
+      await this.openModal()
+    } catch (err) {
+      return;
+    }
+
+    this.orders.push({ ...event, notes: [ this.ice, this.sugar ] });
   }
 
   onDelete(index) {
     this.orders = this.orders.filter((order, i) => i !== index);
+  }
+
+  openModal() {
+    return new Promise((resolve, reject) => {
+      this.isShowModal = true;
+      this.modalResult = { resolve, reject };
+    }).then(_ => this.isShowModal = false)
+  }
+
+  closeModal(reason) {
+    if (reason === 0) {
+      this.modalResult.resolve();
+    }
+    this.modalResult.reject();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -15,7 +16,8 @@ import { ComponentModule } from './components';
     AppRoutingModule,
     BrowserModule,
     ComponentModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [
     AppService
